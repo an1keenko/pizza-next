bcacf577c1b07dfe0583ad6f860348f9fcbe01d7
Ihar Anikeyenka <ihar.anikeyenka@n1affiliates.com>
2024-12-19 00:57:57 +0100

    feat: header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
+import { Container, Title } from '@/components/shared';
 
 export default function Home() {
   return (
-    <h1>
-      Home <Button variant="outline">Card</Button>
-    </h1>
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+    </>
   );
 }
