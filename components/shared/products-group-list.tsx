70bdfd5a956a1fd89e4a82f30ee5f49696cf7ae9
Ihar Anikeyenka <ihar.anikeyenka@n1affiliates.com>
2024-12-20 20:26:56 +0100

    feat: categories

---
+++ components/shared/products-group-list.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { useIntersection } from 'react-use';
+
+import { ProductCard, Title } from '@/components/shared';
+import { cn } from '@/lib/utils';
+
+interface Props {
+  title: string;
+  items: any[];
+  categoryId: number;
+  className?: string;
+  listClassName?: string;
+}
+
+export const ProductsGroupList: React.FC<Props> = ({ title, items, categoryId, className, listClassName }) => {
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  useEffect(() => {
+    if (intersection?.isIntersecting) {
+      console.log(title, categoryId);
+    }
+  }, [intersection?.isIntersecting, title, categoryId]);
+
+  return (
+    <div className={className} id={title} ref={intersectionRef}>
+      <Title text={title} size="lg" className="font-extrabold mb-5" />
+      <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+        {items.map((product, i) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            imageUrl={product.imageUrl}
+            price={product.items[0].price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
